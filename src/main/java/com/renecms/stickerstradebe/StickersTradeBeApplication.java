3d3556f1ff033e49f602725a3cf6dfb5cc9ccceb
Rene Soares <renecms@gmail.com>
2022-10-08 13:30:03 -0300

    Created a docker-compose.yml to run the database for development

--- src/main/java/com/renecms/stickerstradebe/StickersTradeBeApplication.java
+++ src/main/java/com/renecms/stickerstradebe/StickersTradeBeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StickersTradeBeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StickersTradeBeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StickersTradeBeApplication.class, args);
+    }
 }
