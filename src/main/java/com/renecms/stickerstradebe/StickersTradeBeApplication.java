3db5f6030dcfc65fbff0e0f655ceb7891f7fb86f
Rene Soares <renecms@gmail.com>
2022-10-08 12:11:08 -0300

    first commit

---
+++ src/main/java/com/renecms/stickerstradebe/StickersTradeBeApplication.java
@@ -0,0 +1,13 @@
+package com.renecms.stickerstradebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickersTradeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickersTradeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renecms/stickerstradebe/StickersTradeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renecms.stickerstradebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickersTradeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
