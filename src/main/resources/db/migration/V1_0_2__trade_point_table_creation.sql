be822aeb6de086b8e777072bd732a69e87647a09
Rene Soares <renecms@gmail.com>
2022-10-09 10:43:24 -0300

    created trade point table

---
+++ src/main/resources/db/migration/V1_0_2__trade_point_table_creation.sql
@@ -0,0 +1,11 @@
+create table if not exists stickers."trade_point"
+(
+    id            SERIAL,
+    name          VARCHAR(50) not null,
+    address         VARCHAR(200) not null,
+    creation_date TIME default now(),
+    constraint trade_point_pk
+        primary key (id),
+    constraint trade_point_unique_name
+        unique (name)
+);
