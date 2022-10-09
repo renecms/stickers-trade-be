c6f8e0f5a9032d209748524172d0ade72816bece
Rene Soares <renecms@gmail.com>
2022-10-09 15:49:07 -0300

    fixing creationDate column type

--- src/main/resources/db/migration/V1_0_1__user_table_creation.sql
+++ src/main/resources/db/migration/V1_0_1__user_table_creation.sql
@@ -3,7 +3,7 @@ create table if not exists stickers."user"
     id            SERIAL,
     name          VARCHAR(50) not null,
     email         VARCHAR(50) not null,
-    creation_date TIME default now(),
+    creation_date timestamp default now(),
     constraint user_pk
         primary key (id),
     constraint user_unique_email
--- src/main/resources/db/migration/V1_0_2__trade_point_table_creation.sql
+++ src/main/resources/db/migration/V1_0_2__trade_point_table_creation.sql
@@ -1,9 +1,9 @@
 create table if not exists stickers."trade_point"
 (
     id            SERIAL,
-    name          VARCHAR(50) not null,
-    address         VARCHAR(200) not null,
-    creation_date TIME default now(),
+    name          VARCHAR(50)  not null,
+    address       VARCHAR(200) not null,
+    creation_date timestamp default now(),
     constraint trade_point_pk
         primary key (id),
     constraint trade_point_unique_name
