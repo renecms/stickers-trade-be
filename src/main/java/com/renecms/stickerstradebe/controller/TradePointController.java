60487725073879c06dd7ad99cd457e132b4ba868
Rene Soares <renecms@gmail.com>
2022-10-09 14:30:07 -0300

    Created endpoints for TradePoint

---
+++ src/main/java/com/renecms/stickerstradebe/controller/TradePointController.java
@@ -0,0 +1,36 @@
+package com.renecms.stickerstradebe.controller;
+
+import com.renecms.stickerstradebe.dto.TradePointDto;
+import com.renecms.stickerstradebe.service.TradePointService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class TradePointController {
+    public TradePointService service;
+
+    @GetMapping("/trade_point/{id}")
+    public TradePointDto get(@PathVariable Integer id) {
+        return service.getTradePoints(id);
+    }
+
+    @GetMapping("/trade_point")
+    public List<TradePointDto> getAll() {
+        return service.getAllTradePoints();
+    }
+
+    @PostMapping("/trade_point")
+    public TradePointDto save(@Valid @RequestBody TradePointDto tradePointDto) {
+        return service.createTradePoint(tradePointDto);
+    }
+
+    @PostMapping("/trade_point/{id}")
+    public TradePointDto update(@PathVariable Integer id, @Valid @RequestBody TradePointDto tradePointDto) {
+        tradePointDto.setId(id);
+        return service.updateTradePoint(tradePointDto);
+    }
+}
---
+++ src/main/java/com/renecms/stickerstradebe/service/TradePointService.java
@@ -0,0 +1,50 @@
+package com.renecms.stickerstradebe.service;
+
+import com.renecms.stickerstradebe.dto.TradePointDto;
+import com.renecms.stickerstradebe.entity.TradePoint;
+import com.renecms.stickerstradebe.repository.TradePointRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class TradePointService {
+    public TradePointRepository tradePointRepository;
+
+    public TradePointDto getTradePoints(Integer id) {
+        return tradePointRepository.findById(id)
+                .map(TradePoint::toDto)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    public TradePointDto updateTradePoint(TradePointDto tradePointDto) {
+        TradePoint tradePointToBeUpdated;
+        Optional<TradePoint> tradePoint = tradePointRepository.findById(tradePointDto.getId());
+        if (tradePoint.isPresent()) {
+            tradePointToBeUpdated = tradePoint.get();
+            tradePointToBeUpdated.setAddress(tradePointDto.getAddress());
+            tradePointToBeUpdated.setName(tradePointDto.getName());
+
+            return tradePointRepository.save(tradePointToBeUpdated).toDto();
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    public TradePointDto createTradePoint(TradePointDto tradePointDto) {
+        TradePoint tradePoint = TradePoint.builder()
+                .name(tradePointDto.getName())
+                .address(tradePointDto.getAddress())
+                .build();
+        return tradePointRepository.save(tradePoint).toDto();
+    }
+
+    public List<TradePointDto> getAllTradePoints() {
+        return tradePointRepository.findAll().stream().map(TradePoint::toDto).collect(Collectors.toList());
+    }
+}
